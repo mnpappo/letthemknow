2a367dec76de74522e44cb938d3611fbb2e0ac9a
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2020-03-22 02:33:20 +0600

    index view added with basic session testing

--- ui/models.py
+++ ui/models.py
@@ -6,7 +6,7 @@ STATE_LIST = (('safe', 'safe'),
 
 
 class Position(models.Model):
-    user_id = models.IntegerField(primary_key=True, auto_created=True)
+    user_id = models.AutoField(primary_key=True)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     state = models.CharField(max_length=50, choices=STATE_LIST, default='safe', help_text='Current State')
--- ui/views.py
+++ ui/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Position
 
-# Create your views here.
-def indec():
-    return 1;
+
+def index(request):
+    user_id = request.session.get('user_id', 0)
+    if user_id == 0:
+        position = Position.objects.create(latitude=12.3333, longitude=13.4444)
+    else:
+        position = Position.objects.get(user_id=user_id)
+
+    request.session['user_id'] = position.user_id
+
+    context = {
+        'user_id': position.user_id,
+    }
+
+    return render(request, 'ui/index.html', context=context)
