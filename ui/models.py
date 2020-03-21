262e68ec0bf899c0ed5db255d0be9249b539cbc3
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2020-03-22 01:51:24 +0600

    add position model to ui app

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+STATE_LIST = (('safe', 'safe'),
+              ('panicked', 'panicked'),
+              ('affected', 'affected'))
+
+
+class Position(models.Model):
+    user_id = models.IntegerField(primary_key=True, auto_created=True)
+    latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    state = models.CharField(max_length=50, choices=STATE_LIST, default='safe', help_text='Current State')
