1fb7438ef202a85bdc3b01b7f930a57e2efca276
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2020-03-22 20:29:58 +0600

    modified view function

--- ui/views.py
+++ ui/views.py
@@ -5,6 +5,7 @@ from .models import Position
 def index(request):
     user_id = request.session.get('user_id', 0)
     if user_id == 0:
+
         position = Position.objects.create(latitude=12.3333, longitude=13.4444)
     else:
         position = Position.objects.get(user_id=user_id)
