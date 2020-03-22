b10d82a805b0b5730ffb2b5f791867094b948df4
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2020-03-22 20:55:23 +0600

    division added and migrated in position model

--- ui/models.py
+++ ui/models.py
@@ -4,9 +4,20 @@ STATE_LIST = (('safe', 'safe'),
               ('panicked', 'panicked'),
               ('affected', 'affected'))
 
+DIVISION_LIST = (('dhaka', 'Dhaka'),
+                 ('chittagong', 'Chittagong'),
+                 ('barisal', 'Barisal'),
+                 ('khulna', 'Khulna'),
+                 ('mymensingh', 'Mymensingh'),
+                 ('rajshahi', 'Rajshahi'),
+                 ('rangpur', 'Rangpur'),
+                 ('sylhet', 'Sylhet'),
+                 )
+
 
 class Position(models.Model):
     user_id = models.AutoField(primary_key=True)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
-    state = models.CharField(max_length=50, choices=STATE_LIST, default='safe', help_text='Current State')
+    state = models.CharField(max_length=50, choices=STATE_LIST, blank=True, null=True)
+    division = models.CharField(max_length=50, choices=DIVISION_LIST, null=True, blank=True)
