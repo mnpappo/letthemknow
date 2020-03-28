145fa792716f50f9e3be03acfcb8e8329e7ac886
Nadimozzaman Pappo <mnpappo@gmail.com>
2020-03-28 06:39:42 +0600

    test

--- letthemknow/settings.py
+++ letthemknow/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'letthemknow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'letthemknow',
-        'USER': 'letthemknowuser',
-        'PASSWORD': '1111',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'letthemknow',
+#         'USER': 'letthemknowuser',
+#         'PASSWORD': '1111',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
